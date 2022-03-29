1d15011e683e275ba48d3cb7aebedcd1a2e983d6
Bruna Sofia Oliveira Ribeiro <up201804925@fe.up.pt>
2022-03-29 22:09:01 +0000

    normals ok (ex3)

--- tp3/MyParallelogram.js
+++ tp3/MyParallelogram.js
@@ -14,17 +14,33 @@ export class MyParallelogram extends CGFobject {
 		this.vertices = [
 			0, 0, 0,	//0
 			1, 1, 0,    //1
-			2, 0, 0,     //2
-			3, 1, 0     //3
+			2, 0, 0,    //2
+			3, 1, 0,    //3
+
+			0, 0, 0,	//4
+			1, 1, 0,    //5
+			2, 0, 0,    //6
+			3, 1, 0     //7
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
-			2, 1, 0,
+			0, 2, 1,
 			3, 1, 2,
-			0, 1, 2,
-			2, 1, 3,
+			5, 6, 4,
+			6, 5, 7
 		];
+
+		this.normals = [
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
+		]
 
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles

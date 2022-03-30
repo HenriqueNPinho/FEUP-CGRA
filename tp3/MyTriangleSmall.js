019fb4bd9b9d1f17172195a8d5e9d89f51b6054e
Bruna Sofia Oliveira Ribeiro <up201804925@fe.up.pt>
2022-03-30 15:16:01 +0000

    Update MyTriangleSmall.js

--- tp3/MyTriangleSmall.js
+++ tp3/MyTriangleSmall.js
@@ -19,14 +19,13 @@ export class MyTriangleSmall extends CGFobject {
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
-            0, 1, 2,
-            2, 1, 0,  
+            0, 1, 2, 
 		];
 
 		this.normals = [
-			0, 0, -1,
-			0, 0, -1,
-			0, 0, -1
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
 		];
 
 		//The defined indices (and corresponding vertices)

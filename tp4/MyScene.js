4fb78b8c87e56f0b9306071f43f141f558a165c2
Henrique <up201805000@up.pt>
2022-04-04 23:24:03 +0100

    small changes

--- tp4/MyScene.js
+++ tp4/MyScene.js
@@ -79,6 +79,27 @@ export class MyScene extends CGFscene {
     initCameras() {
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
     }
+    hexToRgbA(hex)
+    {
+        var ret;
+        //either we receive a html/css color or a RGB vector
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            ret=[
+                parseInt(hex.substring(1,3),16).toPrecision()/255.0,
+                parseInt(hex.substring(3,5),16).toPrecision()/255.0,
+                parseInt(hex.substring(5,7),16).toPrecision()/255.0,
+                1.0
+            ];
+        }
+        else
+            ret=[
+                hex[0].toPrecision()/255.0,
+                hex[1].toPrecision()/255.0,
+                hex[2].toPrecision()/255.0,
+                1.0
+            ];
+        return ret;
+    }
 
     setDefaultAppearance() {
         this.setAmbient(0.2, 0.4, 0.8, 1.0);
@@ -133,7 +154,7 @@ export class MyScene extends CGFscene {
         // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
         if(this.selectedObject!=-1){
             if( this.selectedObject ==0) this.quadMaterial.apply();
-            this.objects[this.selectedObject].display()
+            this.objects[this.selectedObject].display();
         }
             
         

a468dd6de673e9e2fe512128ace2132909c5be86
Bruna Sofia Oliveira Ribeiro <up201804925@fe.up.pt>
2022-05-21 13:50:05 +0000

    Update MySphere.js

--- project/MySphere.js
+++ project/MySphere.js
@@ -1,13 +1,5 @@
 import { CGFobject } from '../lib/CGF.js';
 
-/**
-* MySphere
-* constructor
-* @param  {CGFscene} scene - MyScene object
-* @param  {integer} slices - number of slices around Y axis
-* @param  {integer} stacks - number of stacks along Y axis, from the center to the poles (half of sphere)
-*/
-
 export class MySphere extends CGFobject {
   /**
    * @method constructor
@@ -23,11 +15,6 @@ export class MySphere extends CGFobject {
     this.initBuffers();
   }
 
-  /**
-   * @method initBuffers
-   * Initializes the sphere buffers
-   * TODO: DEFINE TEXTURE COORDINATES
-   */
   initBuffers() {
     this.vertices = [];
     this.indices = [];
@@ -36,50 +23,34 @@ export class MySphere extends CGFobject {
 
     var phi = 0;
     var theta = 0;
-    var phiInc = Math.PI / this.latDivs;
-    var thetaInc = (2 * Math.PI) / this.longDivs;
-    var latVertices = this.longDivs + 1;
 
-    // build an all-around stack at a time, starting on "north pole" and proceeding "south"
     for (let latitude = 0; latitude <= this.latDivs; latitude++) {
-      var sinPhi = Math.sin(phi);
-      var cosPhi = Math.cos(phi);
 
-      // in each stack, build all the slices around, starting on longitude 0
       theta = 0;
       for (let longitude = 0; longitude <= this.longDivs; longitude++) {
-        //--- Vertices coordinates
-        var x = Math.cos(theta) * sinPhi;
-        var y = cosPhi;
-        var z = Math.sin(-theta) * sinPhi;
+        //vertices
+        var x = Math.cos(theta) * (Math.sin(phi));
+        var y = Math.cos(phi);
+        var z = Math.sin(-theta) * (Math.sin(phi));
         this.vertices.push(x, y, z);
 
-        //--- Indices
+        //indices
         if (latitude < this.latDivs && longitude < this.longDivs) {
-          var current = latitude * latVertices + longitude;
-          var next = current + latVertices;
-          // pushing two triangles using indices from this round (current, current+1)
-          // and the ones directly south (next, next+1)
-          // (i.e. one full round of slices ahead)
+          var current = latitude * (this.longDivs + 1) + longitude;
+          var next = current + (this.longDivs + 1);
 
           this.indices.push(current + 1, current, next);
           this.indices.push(current + 1, next, next + 1);
         }
 
-        //--- Normals
-        // at each vertex, the direction of the normal is equal to 
-        // the vector from the center of the sphere to the vertex.
-        // in a sphere of radius equal to one, the vector length is one.
-        // therefore, the value of the normal is equal to the position vectro
+        //normais
         this.normals.push(x, y, z);
-        theta += thetaInc;
+        theta += ((2 * Math.PI) / this.longDivs);
 
-        //--- Texture Coordinates
-        
         this.texCoords.push(longitude / this.longDivs, latitude / this.latDivs);
       }
 
-      phi += phiInc;
+      phi += (Math.PI / this.latDivs);
     }
 
     this.primitiveType = this.scene.gl.TRIANGLES;

6fd69c79847e9695dc80369400801d1f243897e7
Henrique <up201805000@up.pt>
2022-05-28 18:13:08 +0100

    update

--- project/MyMovingTrain.js
+++ project/MyMovingTrain.js
@@ -20,7 +20,7 @@ export class MyMovingTrain extends CGFobject {
         this.position =[this.track.segs[this.currSeg].x, 0,this.track.segs[this.currSeg].z]
         this.orientation = 0;
         this.lastT = 0;
-        this.totaldis = 0;
+        this.distAtual = 0;
 
         console.log(this.track.segs)
         console.log("(x,z) : (" + this.track.segs[this.currSeg].x + "," + this.track.segs[this.currSeg].z + ")")
@@ -31,8 +31,10 @@ export class MyMovingTrain extends CGFobject {
 
     update(t){ //50 millis, max speed 0,01/sec -> 0,0005 /milli
     
-        let currSeg = this.track.segs[this.currSeg];
+        let seg = this.track.segs[this.currSeg];
+        let segDist = Math.sqrt( Math.pow((seg.x-seg.x1),2) + Math.pow((seg.z-seg.z1),2) )
         let nxtSeg;
+        this.speed = 1; //0.01 é demais
        /* if(this.currSeg+1 < this.track.segs.length){
             nxtSeg=this.track.segs[this.currSeg++];
         }
@@ -41,20 +43,25 @@ export class MyMovingTrain extends CGFobject {
              nxtSeg=this.track.segs[0];
             
         }*/
-        if(this.lastT!=0){
-        this.orientation = Math.atan2(currSeg.z1-currSeg.z,currSeg.x1-currSeg.x);
-
-        let dir= [ currSeg.x1 - currSeg.x, currSeg.z1-currSeg.z]
-        let deltatime= (t-this.lastT)/1000
-        let dirx= dir[0] * deltatime * this.speed
-        let dirz=dir[1]*deltatime* this.speed
         
-        console.log("delta" ,this.orientation)
-  
+        if(this.lastT!=0){
+            this.orientation = Math.atan2(seg.z1-seg.z,seg.x1-seg.x);
 
+            let dir= [ seg.x1 - seg.x, seg.z1-seg.z]
+            let deltatime= (t-this.lastT)/1000
+            let dirx= dir[0] * deltatime * this.speed
+            let dirz=dir[1]*deltatime* this.speed
+            this.distAtual+= Math.sqrt( Math.pow(dirx,2) + Math.pow(dirz,2) )
+            
+            this.position=[this.position[0]+dirx, this.position[1],this.position[2]+dirz]
 
-        this.position=[this.position[0]+dirx, this.position[1],this.position[2]+dirz ]
-        console.log(this.position)
+            if(this.distAtual +0.04 > segDist){
+                this.currSeg++
+                this.distAtual=0;
+            }
+            if(this.currSeg == this.track.segs.length){
+                this.currSeg=0;
+            }
         }
 
         this.lastT=t;
